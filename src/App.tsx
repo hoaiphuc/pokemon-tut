c74f5d17f7e9862946524d03e9e6b13a1a18a78b
Hoai Phuc <nguyenhoaiphuc3122001@gmail.com>
2023-10-19 14:38:25 +0700

    show poke, detail, load more

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import PokemonCollection from './components/PokemonCollection';
+import { Detail, Pokemon } from './interface';
 
-function App() {
+interface Pokemons {
+  name: string;
+  url: string;
+}
+
+
+
+const App: React.FC = () => {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [nextUrl, setNextUrl] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [viewDetail, setDetail] = useState<Detail>({
+    id: 0,
+    isOpened: false,
+  })
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+      );
+      setNextUrl(res.data.next);
+      res.data.results.forEach(async (pokemon: Pokemons) => {
+        const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+        setPokemon((p) => [...p, poke.data]);
+        setLoading(false);
+      })
+    };
+    getPokemon()
+  }, []);
+
+  const nextPage = async () => {
+    setLoading(true);
+    let res = await axios.get(nextUrl);
+    setNextUrl(res.data.next);
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+      setPokemon((p) => [...p, poke.data]);
+      setLoading(false);
+    })
+  }
+
   return (
     <div className="App">
+      <div className="container">
+        <header className="pokemon-header">
+          Pokemon
+        </header>
+        <PokemonCollection pokemons={pokemon} viewDetail={viewDetail} setDetail={setDetail} />
+        {!viewDetail.isOpened && (
+          <div className="btn">
+            <button onClick={nextPage}>{loading ? "Loading ..." : "Load More"}</button>
+          </div>
+        )}
+
+      </div>
     </div>
   );
 }
